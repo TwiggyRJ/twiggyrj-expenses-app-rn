538c395a3e454905d9d330fe77b476a6f7bfccad
Aaron Fryer <aaronfvaio@msn.com>
2017-08-25 16:12:06 +0100

    BG Video component now takes a video prop

--- app/routes/login/login.js
+++ app/routes/login/login.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Text, View, Image, TextInput } from 'react-native';
 import styles from './styles';
+import videos from '../../config/videos';
 import BGVideo from '../../components/bg-video/index';
 import Button from '../../components/button/index';
 
 const Login = (props) => {
   return (
     <View style={styles.container}>
-      <BGVideo />
+      <BGVideo video={videos.loginBackground}/>
       <TextInput style={styles.textInput} placeholder={'Enter email address'} placeholderTextColor={"#FFF"} />
       <TextInput style={styles.textInput} placeholder={'Enter password'} placeholderTextColor={"#FFF"} secureTextEntry={true} />
       <Button style={styles.loginButton} text="Sign In" onPress={() => props.authenticate('Foo', 'Bar')}/>
