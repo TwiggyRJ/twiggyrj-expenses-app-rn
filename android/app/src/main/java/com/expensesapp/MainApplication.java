73659e00cef902236847fd26f800e70599d73458
Aaron Fryer <aaronfvaio@msn.com>
2017-09-11 22:25:43 +0100

    Added .env and async storage library

--- android/app/src/main/java/com/expensesapp/MainApplication.java
+++ android/app/src/main/java/com/expensesapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.expensesapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ReactNativeConfigPackage()
       );
     }
   };
@@ -54,7 +56,8 @@ public class MainApplication extends NavigationApplication {
     // No need to add RnnPackage and MainReactPackage
     return Arrays.<ReactPackage>asList(
       // eg. new VectorIconsPackage()
-      new ReactVideoPackage()
+      new ReactVideoPackage(),
+      new ReactNativeConfigPackage()
     );
   }
 
---
+++ app/config/client.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+export const api = axios.create({
+  baseURL: 'http://192.168.0.47/~aaron/ExpensesWebApp/public/',
+  headers: {
+    'Content-Type' : 'application/json',
+    'Accept' : 'application/json',
+  }
+});
