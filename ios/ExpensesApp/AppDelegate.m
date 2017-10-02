f0a726aff831a383675c28c6c379207082edf37a
Aaron Fryer <aaronfvaio@msn.com>
2017-10-02 22:10:37 +0100

    Fixes for iOS build issues

--- ios/ExpensesApp/AppDelegate.m
+++ ios/ExpensesApp/AppDelegate.m
@@ -12,10 +12,40 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import "RCCManager.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
+  NSURL *jsCodeLocation;
+  
+#ifdef DEBUG
+  
+  //  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  
+#else
+  
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  
+#endif
+  
+  
+  // **********************************************
+  
+  // *** DON'T MISS: THIS IS HOW WE BOOTSTRAP *****
+  
+  // **********************************************
+  
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  
+  self.window.backgroundColor = [UIColor whiteColor];
+  
+  [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation launchOptions:launchOptions];
+  
+  /*
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
@@ -31,6 +61,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+   */
   return YES;
 }
 
