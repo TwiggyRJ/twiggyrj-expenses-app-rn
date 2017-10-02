8435df6c5611c12c8408503eb409725b80e78c95
Aaron Fryer <aaronfvaio@msn.com>
2017-10-02 21:46:48 +0100

    fix for react native navigation

--- android/app/src/main/java/com/aaronfryer/budgiet/MainApplication.java
+++ android/app/src/main/java/com/aaronfryer/budgiet/MainApplication.java
@@ -4,16 +4,13 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.brentvatne.react.ReactVideoPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactnativenavigation.NavigationReactPackage;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.reactnativenavigation.NavigationApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.brentvatne.react.ReactVideoPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -51,7 +48,6 @@ public class MainApplication extends Application implements ReactApplication {
     SoLoader.init(this, /* native exopackage  false);
   }
 }*/
-import com.reactnativenavigation.NavigationApplication;
 
 public class MainApplication extends NavigationApplication {
 
@@ -65,6 +61,7 @@ public class MainApplication extends NavigationApplication {
     // Add additional packages you require here
     // No need to add RnnPackage and MainReactPackage
     return Arrays.<ReactPackage>asList(
+      //new NavigationReactPackage(),
       new VectorIconsPackage(),
       new ReactVideoPackage(),
       new ReactNativeConfigPackage()
